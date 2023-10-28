379dbbf51ee2cc0449ad3024279ef1e4eb4264f2
Lucas Jimenez <lucasjimenezk1998@gmail.com>
2023-10-28 00:21:23 -0600

    authRoutes complete

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,21 +1,76 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const bcrypt = require('bcrypt')
+const User = require('../models/models').User
 
 
-router.post('/register', (req, res) => {
+console.log('runnin')
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/auth')
+  }
+  next()
+}
+
+
+
+router.post('/register', checkNotAuthenticated, async (req, res) =>{
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const newUser = new User({
+            name: req.body.name,
+            email:req.body.email,
+            password:hashedPassword,
+        })
+        await newUser.save();
+        res.redirect('/auth/login')
+    } catch {
+      res.redirect('/auth/register')
+    }
+  })
+
+  router.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+router.get('/profile', checkAuthenticated, (req, res) => {
+  res.send(`Welcome, ${req.user.username}!`);
 });
 
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/login',
-  failureFlash: true
-}));
+router.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+
+
+router.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/auth',
+    failureRedirect: '/login',
+    failureFlash:true
+}))
 
 
 router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
+  req.logout((err) => {
+    if (err) {
+      // Handle the error, if any
+      console.error(err);
+    }
+    res.redirect('/auth/login');
+  });
 });
 
+router.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', {name: req.user.name})
+})
+
 module.exports = router;
+
--- server/server.js
+++ server/server.js
@@ -4,21 +4,18 @@ if (process.env.NODE_ENV !== 'production') {
 require('./db/connect')
 const express = require('express')
 const app = express()
-require('dotenv').config
 const questionRoutes = require('./routes/questionRoutes');
 const answerRoutes = require('./routes/answerRoutes');
 const path = require('path');
-const bcrypt = require('bcrypt')
-const User = require('./models/models').User;
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy;
-const expressSession = require('express-session');
 const crypto = require('crypto');
 const secretKey = crypto.randomBytes(32).toString('hex');
 const flash = require('express-flash')
-const session = require('express-session')
+const expressSession = require('express-session')
 const initializePassport = require('../passport-config');
 const methodOverride = require('method-override')
+const authRoutes = require('./routes/authRoutes')
 
 
 initializePassport(passport)
@@ -34,9 +31,10 @@ const start = async () =>{
 }
 
 app.set('view engine', 'ejs');
+app.set('views' , path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
-app.use(session({
+app.use(expressSession({
   secret: process.env.SESSION_SECRET,
   resave: false, 
   saveUninitialized: false
@@ -44,92 +42,12 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride('_method'))
-
-
-app.set('views' , path.join(__dirname, 'views'));
-
-app.use(expressSession({
-  secret: secretKey,
-  resave: true,
-  saveUninitialized: true
-}));
-
-
-
-
-function isAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login'); 
-}
-
-
-
-
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', {name: req.user.name})
-})
-
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-})
-
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash:true
-}))
-
-
-app.get('/register', (req, res) => {
-    res.render('register.ejs')
-})
-
-app.post('/register', checkNotAuthenticated, async (req, res) =>{
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        const newUser = new User ({
-            name: req.body.name,
-            email:req.body.email,
-            password:hashedPassword,
-        })
-        await newUser.save();
-        res.status(201).json({ message: 'User registered successfully' });
-        res.redirect('/login')
-    } catch {
-      res.redirect('/register')
-    }
-  })
-
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
-})
-
-app.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/login.ejs');
-});
-
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-
-  res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/')
-  }
-  next()
-}
-
 app.use(express.json());
 
+app.use('/auth', authRoutes);
 app.use('/api', questionRoutes);
+
+
  
 start()
 
